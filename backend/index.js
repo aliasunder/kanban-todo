322d4cd91e4438dc38c4dc5a8d51294f2c577c92
aliasunder <32620895+aliasunder@users.noreply.github.com>
2018-08-22 01:56:00 -0400

    queries for finding all todos, and todos filtered by status and id complete. Mutation for adding todos also complete

--- backend/index.js
+++ backend/index.js
@@ -22,18 +22,21 @@ const typeDefs = gql`
    type ToDo { 
       title: String!, 
       description: String!, 
-      status: Boolean, 
+      status: Boolean!, 
       due_date: String!,
-      id: ID
+      id: ID!
    }
    type Query { 
       todos: [ToDo] 
+      filterByStatus(status: Boolean): [ToDo]
+      filterById(id: ID): [ToDo]
+      todo(id: ID!) : ToDo
    }
    input ToDoInput {
       title: String!, 
       description: String!, 
       status: Boolean! = false, 
-      due_date: String!
+      due_date: String!,
    }
    type Mutation {
       createNewToDo(input: ToDoInput) : ToDo
@@ -46,13 +49,30 @@ const resolvers = {
       todos: async () => {
          const todos = await knex('todos').select()
          return todos;
+      },
+      filterByStatus: async (_, { status }) => {
+         console.log(status)
+         const filtered = await knex('todos')
+            .where('status', '=', status)
+         return filtered;
+      },
+      filterById: async (_, { id }) => {
+         console.log(id)
+         const filtered = await knex('todos')
+            .where('id', '=', id)
+         return filtered;
+      },
+      todo: async () => {
+         const todo = await knex.select('id').from('todos')
+         return todo;
       }
    },
    Mutation: {
       createNewToDo: async (_, { input }) => {
-      return await knex('todos')
-         .returning('id')
+      const [newToDo]= await knex('todos')
+         .returning(['id', 'title', 'description', 'status', 'due_date'])
          .insert(input)
+         return newToDo;
       },
       updateToDo: async (_, { id, input }) => {
          const updatedToDo = await database('todos')
@@ -67,5 +87,5 @@ const server = new ApolloServer({ typeDefs, resolvers});
 server.applyMiddleware({ app })
 
 app.listen({ port: PORT }, () => {
-  console.log(`Go to http://${HOST}:${PORT}/graphiql to run queries!`)
+  console.log(`Go to http://${HOST}:${PORT}/graphql to run queries!`)
 })
